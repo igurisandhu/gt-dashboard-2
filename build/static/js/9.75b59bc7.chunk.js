b003c634844f23b9a0b954d4eccac2c9fe240d7d
igurisandhu <igurisandhu@yahoo.com>
2024-08-20 00:17:04 +0530

    build: sprint 3 updated

---
+++ build/static/js/9.75b59bc7.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonplightence-admin"]=this["webpackJsonplightence-admin"]||[]).push([[9],{649:function(e,t,c){"use strict";c.r(t);var n=c(0),s=c(22),b=c(14),i=c(20),j=c(2);t.default=()=>{const e=Object(s.q)(),t=Object(i.b)((e=>e.auth.token));return Object(n.useEffect)((()=>{t&&e("/")}),[]),Object(j.jsx)(b.r,{children:Object(j.jsx)(b.d,{children:Object(j.jsx)(b.m,{children:Object(j.jsx)(s.b,{})})})})}}}]);
+//# sourceMappingURL=9.75b59bc7.chunk.js.map
